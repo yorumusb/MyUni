13fdcfc24b201e3ffdd4aff34a9dbf8f449b40fc
yorumusb <yorumusb@gmail.com>
2024-08-17 17:53:28 +0300

    change service class

--- lib/features/universities/services/university_service.dart
+++ lib/features/universities/services/university_service.dart
@@ -1,9 +1,19 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:my_uni/features/universities/models/university_model.dart';
 
 class UniversityService {
-  Future<http.Response> getAllUniversities() {
-    return http.get(
-      Uri.parse("http://universities.hipolabs.com/search?country=turkey"),
+  Future<List<UniversityModel>> fetchUniversities() async {
+    final response = await http.get(
+      Uri.parse('http://universities.hipolabs.com/search?country=turkey'),
     );
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      return data.map((json) => UniversityModel.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load universities');
+    }
   }
 }
