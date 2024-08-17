25766b734fd9ab584641ad4729d691aef0a518ab
yorumusb <yorumusb@gmail.com>
2024-08-17 18:34:08 +0300

    add widgets part

--- lib/features/universities/presentation/home/home_page.dart
+++ lib/features/universities/presentation/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_uni/features/universities/presentation/home/cubits/university_cubit.dart';
 import 'package:my_uni/features/universities/presentation/home/cubits/university_state.dart';
+import 'package:my_uni/features/universities/presentation/home/widgets/custom_loading.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,34 +12,46 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: customAppBar(),
       body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: BlocBuilder<UniversityCubit, UniversityState>(
-            builder: (context, state) {
-              if (state is UniversityLoading) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (state is UniversityLoaded) {
-                return ListView.builder(
-                  itemCount: state.universities.length,
-                  itemBuilder: (context, index) {
-                    final university = state.universities[index];
-                    return ListTile(
-                      title: Text(university.name),
-                    );
-                  },
-                );
-              } else if (state is UniversityError) {
-                return Center(
-                  child: Text("Failed to load universities: ${state.error}"),
-                );
-              } else {
-                return const Center(
-                  child: Text("Please wait..."),
-                );
-              }
-            },
-          )),
+        padding: const EdgeInsets.all(15.0),
+        child: BlocBuilder<UniversityCubit, UniversityState>(
+          builder: (context, state) {
+            if (state is UniversityLoading) {
+              return const CustomLoading();
+            } else if (state is UniversityLoaded) {
+              return universityList(state);
+            } else if (state is UniversityError) {
+              return Center(
+                child: Text("Failed to load universities: ${state.error}"),
+              );
+            } else {
+              return const Center(
+                child: Text("Please wait..."),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  ListView universityList(UniversityLoaded state) {
+    return ListView.builder(
+      itemCount: state.universities.length,
+      itemBuilder: (context, index) {
+        final university = state.universities[index];
+        return Card(
+          child: ListTile(
+            title: Text(
+              university.name,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            trailing: const Icon(
+              Icons.arrow_right,
+              color: Colors.grey,
+            ),
+          ),
+        );
+      },
     );
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
       home: const HomePage(),
